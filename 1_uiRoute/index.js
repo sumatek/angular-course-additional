aeff0f123eeeb4f659358b004a5dabd760d7dcce
Sumate Kitraunpiromsuk <SumateK@tac.co.th>
2015-11-24 11:16:02 +0700

    add ui-route

--- 1_uiRoute/index.js
+++ 1_uiRoute/index.js
@@ -1,7 +1,24 @@
 angular.module("uiRouterExample", ["ui.router"])
 
 .config(function($stateProvider, $urlRouterProvider) {
-  
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'templates/home.html'
+    })
+    .state('list', {
+      url: '/list',
+      controller: 'ListCtrl',
+      templateUrl: 'templates/list.html'
+    })
+    .state('list.item', {
+      url: '/:item',
+      templateUrl: 'templates/list.item.html',
+      controller: function($scope, $stateParams) {
+        $scope.item = $stateParams.item;
+      }
+    })
+
   $urlRouterProvider.otherwise('/home');
 })
 
